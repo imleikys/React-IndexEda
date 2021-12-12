8a0239d613908532a48b3cdaf1252df85132ea04
Danila Julev <daniljulev@gmail.com>
2021-12-12 17:21:27 +0300

    add(modal): add fake data for modal components

--- src/components/DeliveryBlock/DeliveryBlockCard/DeliveryBlockCard.jsx
+++ src/components/DeliveryBlock/DeliveryBlockCard/DeliveryBlockCard.jsx
@@ -1,6 +1,26 @@
+import {useContext} from "react";
+import {useDispatch} from "react-redux";
+import AppContext from "../../../context";
+import {clearModal, setItemsForModal} from "../../../redux/actions/modal";
+
+
 export const DeliveryBlockCard = ({img, title, className}) => {
+  const dispatch = useDispatch();
+  const {foodCards, setIsModalActive} = useContext(AppContext);
+
+  const onClickHandler = () => {
+    dispatch(clearModal());
+
+    let itemsForModal;
+    if (foodCards.length !== 0) {
+      itemsForModal = foodCards.filter((card) => card.id % 4 === 0);
+      dispatch(setItemsForModal(itemsForModal));
+      setIsModalActive(true);
+    }
+  }
+
   return (
-    <div className="delivery-card">
+    <div onClick={onClickHandler} className="delivery-card">
       <img src={img} alt="Index.Eda: FoodCard" className="delivery-card__img"/>
       <span className={`delivery-card__tag ${className}`}>{title}</span>
     </div>
--- src/components/PromoBlock/PromoBlockCard/PromoBlockCard.jsx
+++ src/components/PromoBlock/PromoBlockCard/PromoBlockCard.jsx
@@ -1,4 +1,24 @@
+import {useContext} from "react";
+import {useDispatch} from "react-redux";
+import AppContext from "../../../context";
+import {clearModal, setItemsForModal} from "../../../redux/actions/modal";
+
+
 export const PromoBlockCard = ({price, title, img, deliveryPrice, descr}) => {
+  const dispatch = useDispatch()
+  const {foodCards, setIsModalActive} = useContext(AppContext);
+
+  const onClickHandler = () => {
+    dispatch(clearModal());
+    
+    let itemsForModal;
+    if (foodCards.length !== 0) {
+      itemsForModal = foodCards.filter((card) => card.id % 3 === 0);
+      dispatch(setItemsForModal(itemsForModal));
+      setIsModalActive(true);
+    }
+  }
+
   return (
     <div className="main-promo__card">
         <img className="card-img" src={img} alt="Index.Eda: SweetBasket" />
@@ -9,7 +29,7 @@ export const PromoBlockCard = ({price, title, img, deliveryPrice, descr}) => {
         <div className="card-footer">
           <strong className="card-price">ОТ {price}</strong>
           <span className="card-delivery">стоимость доставки до {deliveryPrice}</span>
-          <a className="button button-default" href="/">Собрать корзину</a>
+          <button onClick={onClickHandler} className="button button-default" href="/">Собрать корзину</button>
         </div>
     </div>
   )
